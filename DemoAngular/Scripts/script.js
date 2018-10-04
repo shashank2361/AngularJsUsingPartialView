f5d839d6b2ee01f24c68d33ed5e4a9cc5b75bbb6
DESKTOP-SB3UMDG\Shashank-PC <Shashank2361@yahoo.co.in>
2018-10-04 16:16:46 +0100

    FirstCommit

---
+++ DemoAngular/Scripts/script.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="angular.js" />
+/// <reference path="angular-route.js" />
+ 
+var app = angular
+    .module("Demo", ["ngRoute"])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when("/home", {
+                templateUrl: "Templates/home.html",
+                controller: "homeController"
+            })
+            .when("/courses", {
+                templateUrl: "Templates/courses.html",
+                controller: "coursesController"
+            })
+            .when("/students", {
+                templateUrl: "Templates/students.html",
+                controller: "studentsController"
+            })
+            .when("/students/:id", {
+                templateUrl: "Templates/studentDetails.html",
+                controller: "studentDetailsController"
+            })
+            .otherwise({
+                redirectTo:"/home"
+            })
+        $locationProvider.html5Mode(true);
+    })
+app.controller("homeController", function ($scope) {
+         $scope.message = "Home Page";
+    })
+app.controller("coursesController", function ($scope) {
+        $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
+    })
+app.controller("studentsController", function ($scope, $http) {
+        $http.get("StudentService.asmx/GetAllStudents")
+            .then(function (response) {
+                $scope.students = response.data;
+            })
+    })
+
+app.controller("studentDetailsController", function ($scope, $http, $routeParams) {
+    $http({
+        url: "StudentService.asmx/GetStudent",
+        method: "get",
+        params: { id: $routeParams.id }
+    }).then(function (response) {
+        $scope.student = response.data;
+    })
+})
---
+++ DemoAngular/StudentService.asmx.cs
@@ -0,0 +1,92 @@
+﻿using AngularJsDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+
+namespace AngularJsDemo
+{
+    /// <summary>
+    /// Summary description for StudentService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class StudentService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+
+        [WebMethod]
+        public void GetAllStudents()
+        {
+            List<Student> listStudents = new List<Student>();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblStudents", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Student student = new Student();
+                    student.id = Convert.ToInt32(rdr["Id"]);
+                    student.name = rdr["Name"].ToString();
+                    student.gender = rdr["Gender"].ToString();
+                    student.city = rdr["City"].ToString();
+                    listStudents.Add(student);
+                }
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(listStudents));
+        }
+
+
+        [WebMethod]
+        public void GetStudent(int id)
+        {
+             Student student = new Student();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblStudents where id = @id", con);
+
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@id",
+                    Value = id
+
+                };
+                cmd.Parameters.Add(param);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    student.id = Convert.ToInt32(rdr["Id"]);
+                    student.name = rdr["Name"].ToString();
+                    student.gender = rdr["Gender"].ToString();
+                    student.city = rdr["City"].ToString();
+                }
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(student));
+        }
+    }
+
+}
+
